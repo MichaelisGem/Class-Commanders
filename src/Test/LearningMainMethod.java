76efa6611b2f84e7de520864c643067df6dd4b54
dakwa <dakwamichael@gmail.com>
2024-08-08 19:20:55 -0400

    Initial commit for my project

--- src/Test/LearningMainMethod.java
+++ src/Test/LearningMainMethod.java
@@ -1,13 +1,15 @@
-package test;
+package Test;
 
 
 
 public class LearningMainMethod {
 
 
-    public static void main(string[] args){
+    public static void main(String[] args){
 
-
+        System.out.println("my name is Michael Dakwa");
+        System.out.println("I love myself");
+        System.out.println("I am 25 years old and was born in USA.");
 
 
     }
